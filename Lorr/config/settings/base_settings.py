287621cb29c9079752e03e2352182c0620d1a52d
OsaeAddo <immanueloaddo@gmail.com>
2022-08-15 18:26:37 +0000

    add permission settings for django rest framework

--- Lorr/config/settings/base_settings.py
+++ Lorr/config/settings/base_settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSIONS_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
