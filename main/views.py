e0327dfa231f7366371dd56bb846cdfc76dad231
OsaeAddo <immanueloaddo@gmail.com>
2022-08-15 15:49:14 +0000

    update views.py

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.utils import timezone
 
 
