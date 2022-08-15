6238873bc3c6096f0a38cf71a2364cdea8a724dc
OsaeAddo <immanueloaddo@gmail.com>
2022-08-15 18:04:39 +0000

    define api view for the item model

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class ItemApiView(generics.ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
