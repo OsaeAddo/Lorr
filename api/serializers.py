69b53c135546bf8d9da7f555c30531f5ea668ce1
OsaeAddo <immanueloaddo@gmail.com>
2022-08-15 18:03:27 +0000

    add serializers module to translate the models into json format for the api endpoints

---
+++ api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+
+from main.models import Item
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = (
+            'item_name', 'price', 'discount_price', 
+            'category', 'label', 'description'
+        )
