5cb1c49701e28c5d6ef3e3bf5d48a9c058169cae
OsaeAddo <immanueloaddo@gmail.com>
2022-08-18 23:40:50 +0000

    add components directory & create Navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="font-bold text-2xl">Navbar</div>
+        </nav>
+    )
+}
+
+export default Navbar;
