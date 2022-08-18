94a9f4ed7363e33606b869e0d0ca99cbd82940b3
OsaeAddo <immanueloaddo@gmail.com>
2022-08-18 23:55:41 +0000

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 
-import {Navbar, Footer} from './components'
+import {Navbar, Footer} from "./components";
 
 
 class App extends Component {
