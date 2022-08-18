7959fcaf254d2c825af69b2c3f1451165bd82d1a
OsaeAddo <immanueloaddo@gmail.com>
2022-08-18 23:45:57 +0000

    add Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+        <nav>
+            <div className="font-bold text-2xl">Footer</div>
+        </nav>
+    )
+}
+
+export default Footer;
