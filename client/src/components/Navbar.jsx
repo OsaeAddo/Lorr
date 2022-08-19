4d837c8126db48eeb8ab515b15a107e498701c32
OsaeAddo <immanueloaddo@gmail.com>
2022-08-19 10:56:32 +0000

    update Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className="w-full items-center justify-center p-4">
             <div className="font-bold text-2xl">Navbar</div>
         </nav>
     )
