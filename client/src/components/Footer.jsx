e290d231437513116536f4da244ccbae4368fdcf
OsaeAddo <immanueloaddo@gmail.com>
2022-08-22 23:33:01 +0000

    update footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { motion } from "framer-motion"
 
 
+import logo from "../assets/logo/lorr-logo.jpeg"
+
 const Footer = () => {
     return (
-        <footer className="grid gap-4 text-center pt-9 pb-4">
+        <footer className="grid gap-4 text-center pt-9 pb-4 bg-gray-900 text-white">
             <div className="grid gap-4 justify-center items-center">
                 <section className="text-start">
                     <h2 className="font-bold text-2xl">Quicklinks</h2>
@@ -45,7 +47,7 @@ const Footer = () => {
             </div>
 
             <div className="">
-                <img src="" alt="L'orr-logo" />
+                <img src={logo} alt="L'orr-logo" />
             </div>
             <div className="">&copy; Copyright 2022</div>
         </footer>
