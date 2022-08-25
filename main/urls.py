68903c174a0a2e4babd0da957040fc0aa2d1703b
OsaeAddo <immanueloaddo@gmail.com>
2022-08-25 16:32:39 +0000

    add url routes for reducing item quantity

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('product/<pk>/', ProductView.as_view(), name="product"),
     path('add-to-cart/', add_to_cart, name="add-to-cart"),
-    path('remove-from-cart/<pk>', remove_from_cart, name="remove-from-cart")
+    path('remove-from-cart/<pk>', remove_from_cart, name="remove-from-cart"),
+    path('order-summary/', OrderSummaryView.as_view(), name="order-summary"),
+    path('reduce-item-quantity/<pk>/', reduce_item_quantity, name="reduce-item-quantity")
+    
 ]
