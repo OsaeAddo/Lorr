85f18cb144684bf1a351970b9e5329d2383be8c3
OsaeAddo <immanueloaddo@gmail.com>
2022-08-25 23:13:18 +0000

    use django-rest-auth & all-auth

--- Lorr/config/settings/base_settings.py
+++ Lorr/config/settings/base_settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #auth
+    'django.contrib.sites',
     
     # my apps
     'main.apps.MainConfig',
