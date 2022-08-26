c821e076a2e77e20c3d974dd60e16d601d31c1d7
OsaeAddo <immanueloaddo@gmail.com>
2022-08-26 20:10:14 +0000

    update settings:

--- Lorr/config/settings/base_settings.py
+++ Lorr/config/settings/base_settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #auth
+    # auth
     'django.contrib.sites',
     
     # my apps
