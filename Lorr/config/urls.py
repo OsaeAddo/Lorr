c5336c101a4215cf02773d53ff39c9c7dcbb5853
OsaeAddo <immanueloaddo@gmail.com>
2022-08-26 20:25:08 +0000

    add django-rest-auth registration urls

--- Lorr/config/urls.py
+++ Lorr/config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     # auth
     path('api-auth/', include('rest_framework.urls')),
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/dj-rest-auth/registration', include('dj_rest_auth.registration.urls')),
 ]
