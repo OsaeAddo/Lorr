d025c555be72403a67e5cad0bfac56eb040fc150
OsaeAddo <immanueloaddo@gmail.com>
2022-08-29 03:52:29 +0000

    update base urls module f

--- Lorr/config/urls.py
+++ Lorr/config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
-    path('api/', include('api.urls')),
+    path('api/v1.0/', include('api.urls')),
     
     # auth
     path('api-auth/', include('rest_framework.urls')),
