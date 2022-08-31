c0dab3fd4b9c37713348ee236769cc8991cea443
OsaeAddo <immanueloaddo@gmail.com>
2022-08-31 21:09:56 +0000

    update bottom bar

--- client/src/components/BottomBar.jsx
+++ client/src/components/BottomBar.jsx
@@ -111,7 +111,7 @@ export default function BottomAppBar() {
           ))}
         </List>
       </Paper>
-      <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+      <AppBar className='' position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
         <Toolbar>
           <IconButton color="inherit" aria-label="open drawer">
             <HomeOutlinedIcon />
